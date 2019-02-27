d7f93b32b1ade777affdd05e6b522b087dfe2449
shadab36 <35285943+shadab36@users.noreply.github.com>
2019-02-27 12:13:30 +0530

    Update BrowserSetup.java

--- src/test/java/browserSetup/BrowserSetup.java
+++ src/test/java/browserSetup/BrowserSetup.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserSetup{
 	
-	//Change test
+	//Change test 13234
 	public static WebDriver driver;
 	public static String AppURL;
 	public static Properties property = new Properties(System.getProperties());
